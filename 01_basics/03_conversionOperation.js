55d3a53c3d97570f44af3b14b8bedf6b2633027c
yashbhosale8105 <165877529+yashbhosale8105@users.noreply.github.com>
2025-11-25 17:14:04 +0000

    conversion and operation commit

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -34,3 +34,36 @@ let someNumber = 33
 
 let stringNumber = String(someNumber) //convert into string
 console.log(typeof stringNumber);
+
+
+// *************************Operation Conversion*************************
+
+let value = 3
+let negValue = -value
+console.log(negValue); //-3
+
+console.log(2+2); //4
+console.log(2-2);
+console.log(2*2);
+console.log(2**2); // 2 to the power 2 => 4
+console.log(2/2);
+console.log(5%2); //gives the remainder => 1
+
+// + operator is also used for concatenation
+let str1 = "Hello " // can give space after hello as well or at the starting of yash
+let str2 = "Yash"
+console.log(str1 + str2); // Hello Yash
+
+
+console.log("1" + 2); // "12"  string concatenation
+console.log(1 + "2"); // "12"
+console.log("1" - 2); // -1  here - works as a mathematical operation
+console.log("1" * "2"); // 2 multiply honar he math operation
+console.log("4" / "2"); // 2
+console.log("1" + 2 + 2); // "122"  jar string asel tar to concatenate honar ani baki sagle string madhe convert honar
+console.log(1 + 2 + "2"); // "32" jar left madhe number asel tar to te add honar nantar string madhe convert honar ani concatenate honar
+
+// minus multiple ani divide direct hota jari string asel or number asel normal ans perform karun yeil
+// fakta addition + madhe jara farak padto string asel tar concatenate karun deto baki madhe mathematical operation perform karto.
+// addition madhe jar pahile string asel tar to concatenate karun deto baki madhe mathematical operation perform karto.
+// ani jar pahile number asel tar to add karun deto nantar string madhe convert karun concatenate karun deto.
